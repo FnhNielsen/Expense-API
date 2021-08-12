f530b1f9b965a2d5e40218cf164c2f44eb2c32ae
Frederik Nielsen <frederik@icsharp.io>
2021-08-12 20:11:16 +0200

    Added a repository with JpaRepository
    Very little code with this one...

---
+++ src/main/java/com/nielsen/expense/services/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package com.nielsen.expense.services;
+
+import com.nielsen.expense.models.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long>
+{
+    
+}
