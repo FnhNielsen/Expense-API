502c9da08312ccd2ee6fa9b0b867b4826c165a9f
Frederik Nielsen <frederik@icsharp.io>
2021-08-12 20:02:52 +0200

    Added Getter and and NoArgsConstructor with Lumbok
    Lumbok is cool btw

--- src/main/java/com/nielsen/expense/models/Expense.java
+++ src/main/java/com/nielsen/expense/models/Expense.java
@@ -1,11 +1,33 @@
 package com.nielsen.expense.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+@NoArgsConstructor
+@With
 public class Expense
 {
+    @Getter
     private int id;
+    @Getter
     private String name;
+    @Getter
     private float price;
-    private DateTimeFormatter expenseDate;
+    @Getter
+    private LocalDateTime expenseDate;
+
+    public Expense(int id, String name, float price, LocalDateTime expenseDate)
+    {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.expenseDate = expenseDate;
+    }
 }
+
+
