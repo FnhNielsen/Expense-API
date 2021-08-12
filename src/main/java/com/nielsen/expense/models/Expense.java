22b0451fd1daa3b1d025bd7ab6302d941bc10139
Frederik Nielsen <frederik@icsharp.io>
2021-08-12 19:56:43 +0200

    init commit

---
+++ src/main/java/com/nielsen/expense/models/Expense.java
@@ -0,0 +1,11 @@
+package com.nielsen.expense.models;
+
+import java.time.format.DateTimeFormatter;
+
+public class Expense
+{
+    private int id;
+    private String name;
+    private float price;
+    private DateTimeFormatter expenseDate;
+}
---
+++ src/test/java/com/nielsen/expense/ExpenseApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nielsen.expense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
